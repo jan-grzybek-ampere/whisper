6856ce3b77c688a391492bdf89bde5affcfa7798
Jan Grzybek <jan@amperecomputing.com>
2023-02-25 02:51:24 +0100

    wip

--- whisper/model.py
+++ whisper/model.py
@@ -9,6 +9,7 @@ from torch import Tensor
 from torch import nn
 
 
+@torch.jit.export
 class ModelDimensions:
     def __init__(self, n_mels: int, n_audio_ctx: int, n_audio_state: int, n_audio_head: int, n_audio_layer: int,
                  n_vocab: int, n_text_ctx: int, n_text_state: int, n_text_head: int, n_text_layer: int):
