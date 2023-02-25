6e4e0e7d0fa0533120984a1c5f7adb963144c044
Jan Grzybek <jan@amperecomputing.com>
2023-02-25 04:04:00 +0100

    wip

--- whisper/decoding.py
+++ whisper/decoding.py
@@ -602,7 +602,7 @@ class DecodingTask:
             # encoded audio features are given; skip audio encoding
             audio_features = mel
         else:
-            audio_features = self.model.encoder(mel)
+            audio_features = self.model.embed_audio(mel)
 
         if audio_features.dtype != (torch.float16 if self.options.fp16 else torch.float32):
             return TypeError(f"audio_features has an incorrect dtype: {audio_features.dtype}")
